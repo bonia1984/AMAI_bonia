b18f38bd0c58e53e2c247a1145413bb70c241b16
bos <b0sus@hotmail.com>
2022-07-03 22:32:10 +0800

    update

--- Bonia1984_AI/amai.exe.bat
+++ Bonia1984_AI/amai.exe.bat
@@ -1,3 +1,2 @@
-del "D:\Games\War3\Maps\Test\(12)DivideAndConquer.w3m"
-copy "D:\Games\War3\Maps\UD\(12)DivideAndConquer.w3m" "D:\Games\War3\Maps\Test\"
+copy "D:\Games\War3\Maps\(12)DivideAndConquer.w3m" "D:\Games\War3\Maps\UD\"
 "D:\Projects\GitHub\AMAI_bonia\Bonia1984_AI\amai.exe"
