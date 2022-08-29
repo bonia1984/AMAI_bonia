f37c5415fef34204a300c7ff02d24c134cdd87b4
bos <b0sus@hotmail.com>
2022-08-29 17:40:46 +0800

    0829

--- Bonia1984_AI/amai.exe.bat
+++ Bonia1984_AI/amai.exe.bat
@@ -1,2 +1,2 @@
-copy "D:\Games\War3\Maps\(12)DivideAndConquer.w3m" "D:\Games\War3\Maps\UD\"
+copy "D:\Games\War3\Maps\Epic\(12)DivideAndConquerTFT.w3x" "D:\Games\War3\Maps\AMAI\"
 "D:\Projects\GitHub\AMAI_bonia\Bonia1984_AI\amai.exe"
